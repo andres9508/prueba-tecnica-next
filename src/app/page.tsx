06c6ca0016e32652f6823b3076bf872379e54b16
andres9508 <64918943+andres9508@users.noreply.github.com>
2024-01-11 11:30:25 -0500

    ajuste de pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,76 +1,29 @@
 "use client";
-import React, { useState } from "react";
-import Image from "next/image";
-import styles from "./page.module.css";
-import { UserInterface } from "@/app/interfaces/User.Interface";
-import Loader from "./Components/Loader";
+import React from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const [user, setUser] = useState<UserInterface>({
-    name: "",
-    birthday: "",
-    gender: "",
-  });
+  const router = useRouter();
 
-  const handleChange = (e: any) => {
-    const { name, value } = e.target;
-
-    setUser({
-      ...user,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e: any) => {
-    console.log(typeof e);
-    e?.preventDefault();
-    // Aquí puedes realizar acciones con los datos del formulario
-    console.log(user);
-  };
   return (
-    <main>
-      <h3>Usuarios</h3>
+    <main className="main">
+      <h3 className="title">Usuarios</h3>
       <div
         style={{
-          flex: 1,
-          height: "full",
-          alignItems: "center",
-          justifyContent: "center",
           display: "flex",
+          flexDirection: "row",
+          gap: 10,
+          width: "100%",
+          maxWidth: 400,
         }}>
-        <form className="my-form" onSubmit={handleSubmit}>
-          <label htmlFor="">
-            Nombre
-            <input
-              type="text"
-              name="name"
-              value={user.name}
-              onChange={handleChange}
-            />
-          </label>
-
-          <label>
-            Cumpleaños:
-            <input
-              type="date"
-              name="birthday"
-              value={user.birthday}
-              onChange={handleChange}
-            />
-          </label>
-
-          <label>
-            Género:
-            <select name="genero" value={user.gender} onChange={handleChange}>
-              <option value="">Seleccione</option>
-              <option value="masculino">Masculino</option>
-              <option value="femenino">Femenino</option>
-              <option value="otro">Otro</option>
-            </select>
-          </label>
-
-          <button type="submit">Enviar</button>
-        </form>
+        <button className="button" onClick={() => router.push("/usuario")}>
+          Usuario
+        </button>
+        <button
+          className="button"
+          onClick={() => router.push("/usuario-consulta")}>
+          Usuario consulta
+        </button>
       </div>
     </main>
   );
